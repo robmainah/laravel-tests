e81f176fd7059675a5d1f95a4c0607295b3405ba
robmainah <robmainah@gmail.com>
2023-03-16 13:29:04 +0300

    added admin lte

--- vue3-tuts/resources/js/app.js
+++ vue3-tuts/resources/js/app.js
@@ -1,5 +1,9 @@
 import './bootstrap';
 
+import "admin-lte/plugins/jquery/jquery.min.js";
+import "admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js";
+import "admin-lte/dist/js/adminlte.min.js";
+
 import {createApp} from 'vue'
 
 import App from './App.vue'
