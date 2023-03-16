6b0d48889cfc1dc3fbe930d0ee4b472c9e5f859c
robmainah <robmainah@gmail.com>
2023-03-16 15:57:49 +0300

    show users from db

--- vue3-tuts/resources/js/pages/users/index.vue
+++ vue3-tuts/resources/js/pages/users/index.vue
@@ -5,7 +5,56 @@
                 <div class="col-sm-6">
                     <h1 class="m-0">Users</h1>
                 </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item">
+                            <router-link to="/dashboard">Home</router-link>
+                        </li>
+                        <li class="breadcrumb-item active">Dashboard</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="content">
+        <div class="container-fluid">
+            <div class="card">
+                <div class="card-body">
+                    <table class="table table-bordered hover">
+                        <thead>
+                            <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Email</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="(user, index) in users" :key="index">
+                                <th>{{ ++index }}</th>
+                                <td>{{ user.name }}</td>
+                                <td>{{ user.email }}</td>
+                            </tr>
+                        </tbody>
+                        </table>
+                </div>
             </div>
         </div>
     </div>
 </template>
+
+<script setup>
+    import { ref, onMounted } from 'vue';
+
+    const users = ref([]);
+
+    const getUsers = () => {
+        axios.get('/api/users').then(data => {
+            users.value = data.data
+        });
+    }
+
+    onMounted(() => {
+        getUsers()
+    });
+</script>
