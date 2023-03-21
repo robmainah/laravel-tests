cc6377ec426808e2cef7bd6bd44a5d5eb5bacc64
robmainah <robmainah@gmail.com>
2023-03-21 12:25:35 +0300

    setup pusher

--- laravel-echo-tuts/resources/js/app.js
+++ laravel-echo-tuts/resources/js/app.js
@@ -1,1 +1,14 @@
 import './bootstrap';
+
+// import {createRouter, createWebHistory} from 'vue-router'
+
+import {createApp} from 'vue'
+// import routes from './routes';
+
+
+// const router = createRouter({
+//     routes,
+//     history: createWebHistory(),
+// });
+
+createApp({}).mount("#app")
